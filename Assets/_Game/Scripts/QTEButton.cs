e439bfd8b5c62c96a857bd27a24cec434474b6cf
sarasehdo <sarasehdo@gmail.com>
2022-02-11 16:41:59 +0100

    Sigh
    Lost everything and done again, a little bit better

--- Assets/_Game/Scripts/QTEButton.cs
+++ Assets/_Game/Scripts/QTEButton.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ButtonScript : MonoBehaviour
-{
+public class QTEButton : MonoBehaviour{
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
 }
