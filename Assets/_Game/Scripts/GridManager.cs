bb4138b6c6d7f11747bbb2113d0f0cf6ee1cf4a1
emmouto <emma.pettersson07@gmail.com>
2022-02-15 12:43:47 +0100

    Match 3 base with selection and not working swapping

--- Assets/_Game/Scripts/GridManager.cs
+++ Assets/_Game/Scripts/GridManager.cs
@@ -3,33 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour {
-    public static GridManager gridManagerInstance;
-    public List<Sprite> tileSprites = new List<Sprite>();
+    public static GridManager instance;
+    public List<Sprite> sprites = new List<Sprite>();
     public GameObject tile; // Prefab
-    public int gridDimensionX = 8;
-    public int gridDimensionY = 8;
+    public int gridX, gridY;
     public float distance = 1.0f;
     private GameObject[,] grid;
+    Sprite[] previousLeft = new Sprite[8];
+    Sprite previousBelow = null;
+    public bool IsShifting {get; set;}
 
     // Start is called before the first frame update
     void Start() {
-        gridManagerInstance = GetComponent<GridManager>(); // Singleton instance of the GridManager
+        instance = GetComponent<GridManager>(); // Singleton instance of the GridManager
 
         Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
         initGrid(offset.x, offset.y);
     }
 
-    // 
+    // Creates a grid with non-repeating tiles.
     private void initGrid(float offsetX, float offsetY) {
-        grid = new GameObject[gridDimensionX, gridDimensionY]; // Create 8x8 grid
+        grid = new GameObject[gridX, gridY]; // Create 8x8 grid
 
         float startX = transform.position.x;
         float startY = transform.position.y;
 
-        for (int x = 0; x < gridDimensionX; x++) {
-            for (int y = 0; y < gridDimensionY; y++) {
+        for (int x = 0; x < gridX; x++) {
+            for (int y = 0; y < gridY; y++) {
                 GameObject newTile = Instantiate(tile, new Vector3(startX + (offsetX * x), startY + (offsetY * y), 0), tile.transform.rotation);
                 grid[x, y] = newTile;
+                newTile.transform.parent = transform;
+
+                List<Sprite> possibleSprites = new List<Sprite>();
+                possibleSprites.AddRange(sprites);
+                possibleSprites.Remove(previousLeft[y]); 
+                possibleSprites.Remove(previousBelow);
+
+                Sprite newSprite = possibleSprites[Random.Range(0, possibleSprites.Count)];
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                previousLeft[y] = newSprite;
+                previousBelow = newSprite;
             }
         }
     }
