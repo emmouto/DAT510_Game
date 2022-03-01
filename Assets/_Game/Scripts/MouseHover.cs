9089306a596574216969cf829d80d6318a59047f
DIONAPERSSON <42834340+DIONAPERSSON@users.noreply.github.com>
2022-03-01 11:15:42 +0100

    Giant crafting update
    Stuff

---
+++ Assets/_Game/Scripts/MouseHover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+           
+    //{
+    //    public static Renderer.material.color = Color.black;
+    //}
+
+    //void OnMouseEnter()
+    //{
+    //    Renderer.material.color = Color.blue;
+    //}
+
+    //void OnMouseExit()
+    //{
+    //    Renderer.material.color = Color.black;
+    //}
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
